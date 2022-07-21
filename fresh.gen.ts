7f88623677442006439786f66835f2711bf80cbe
Yanis <yaberkane.business@gmail.com>
2022-07-21 16:36:52 +0200

    setup of the navbar island done

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,9 +3,8 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $0 from "./routes/api/joke.ts";
-import * as $1 from "./routes/article.tsx";
-import * as $2 from "./routes/exemple.tsx";
-import * as $3 from "./routes/index.tsx";
+import * as $1 from "./routes/exemple.tsx";
+import * as $2 from "./routes/index.tsx";
 import * as $$0 from "./islands/Articles.tsx";
 import * as $$1 from "./islands/Counter.tsx";
 import * as $$2 from "./islands/Footer.tsx";
@@ -18,9 +17,8 @@ import * as $$7 from "./islands/Technologies.tsx";
 const manifest = {
   routes: {
     "./routes/api/joke.ts": $0,
-    "./routes/article.tsx": $1,
-    "./routes/exemple.tsx": $2,
-    "./routes/index.tsx": $3,
+    "./routes/exemple.tsx": $1,
+    "./routes/index.tsx": $2,
   },
   islands: {
     "./islands/Articles.tsx": $$0,
--- routes/index.tsx
+++ routes/index.tsx
@@ -8,11 +8,16 @@ import Header from "../islands/Header.tsx";
 import Projects from "../islands/Projects.tsx";
 import Services from "../islands/Services.tsx";
 import Technologies from "../islands/Technologies.tsx";
-
+import Navbar from "../islands/Navbar.tsx";
 
 export default function Home() {
   return (
     <main>
+      <Navbar links={ [
+        { "name": "Product", "url": "#"  }, 
+        { "name": "Feature", "url": "#"},
+        { "name": "Ressources", "url": "#"}
+        ]}></Navbar>
       <Header description={"Ceci est le portfolio de Yanis"} />
       <Projects></Projects>
       <Articles start={1}></Articles>
